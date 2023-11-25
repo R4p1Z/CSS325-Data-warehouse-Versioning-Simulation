9030b6310f05e421b527ea1d04b0c19f6e93a98b
Chawakorn Chaimongkol <107240946+R4p1Z@users.noreply.github.com>
2023-11-25 12:54:26 +0700

    Adding initial files

---
+++ home.php
@@ -0,0 +1,25 @@
+<?php
+  require_once `connect.php`;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h2>Data Warehouse Versioning Simulator</h2>
+  <br>
+  <table>
+    <thead>
+      <tr>
+        foreach
+      </tr>
+    </thead>
+  </table>
+
+  
+</body>
+</html>
