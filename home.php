9c6bf2ea69ee1a05545a6c9eba8e4a07a997ec04
TtheThanyapak <6422781177@g.siit.tu.ac.th>
2023-11-25 20:33:40 +0700

    ทำหน้าเว็ปมี 6ช่อง มีdropbox

--- home.php
+++ home.php
@@ -8,18 +8,69 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
+  <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-  <h2>Data Warehouse Versioning Simulator</h2>
-  <br>
-  <table>
-    <thead>
-      <tr>
-        foreach
-      </tr>
-    </thead>
-  </table>
-
-  
+  <div class="container">
+    <h2>Data Warehouse Versioning Simulator</h2>
+    <div class="columns">
+      <div class="column">
+        <h3>Add dimension level</h3>
+        <label for="addDL1">Query box:</label>
+        <input type="text" id="addDL1" name="addDL1" placeholder="pls fill in">
+        <div>
+          <input type="submit" class="button btn-small" value="submit">
+        </div>
+      </div>
+      <div class="column">
+        <h3>Add dimension table</h3>
+        <label for="addD2">Query box:</label>
+        <input type="text" id="addD2" name="addD2" placeholder="pls fill in">
+        <div>
+          <input type="submit" class="button btn-small" value="submit">
+        </div>
+      </div>
+      <div class="column">
+        <h3>Update dimension level</h3>
+        <label for="upDL1">Query box:</label>
+        <input type="text" id="upDL1" name="upDL1" placeholder="pls fill in">
+        <div>
+          <input type="submit" class="button btn-small" value="submit">
+        </div>
+      </div>
+      <div class="column">
+        <h3>Update dimension table</h3>
+        <label for="upD2">Query box:</label>
+        <input type="text" id="upD2" name="upD2" placeholder="pls fill in">
+        <div>
+          <input type="submit" class="button btn-small" value="submit">
+        </div>
+      </div>
+      <div class="column">
+        <h3>Delete dimension level</h3>
+        <label for="delDL1">Query box:</label>
+        <input type="text" id="delDL1" name="delDL1" placeholder="pls fill in">
+        <div>
+          <input type="submit" class="button btn-small" value="submit">
+        </div>
+      </div>
+      <div class="column">
+        <h3>Delete dimension table</h3>
+        <label for="delD2">Query box:</label>
+        <input type="text" id="delD2" name="delD2" placeholder="pls fill in">
+        <div>
+          <input type="submit" class="button btn-small" value="submit">
+        </div>
+      </div>
+      <div class="column">
+        <label for="dropdown">Select an option:</label>
+        <select id="dropdown" name="dropdown">
+          <option value="option1">Version 1</option>
+          <option value="option2">Version 2</option>
+          <option value="option3">Version 3</option>
+        </select>
+      </div>
+    </div>
+  </div>
 </body>
 </html>
